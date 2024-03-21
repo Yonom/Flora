d0e3712bfc6dabb47b0d5496a592513c7bd01f70
Simon Farshid <simon.farshid@outlook.com>
2024-03-21 23:26:57 +0100

    Request permission for camera on Android

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FocusFade from "../src/FocusFade";
 import { StyleSheet, View } from "react-native";
 import ScanningIndicator from "../src/ScanningIndicator";
 import ScannerCamera from "../src/ScannerCamera";
+import { useCameraPermission } from "react-native-vision-camera";
 
 export default function App() {
+  const { hasPermission, requestPermission } = useCameraPermission();
+  useEffect(() => {
+    if (!hasPermission) requestPermission();
+  }, []);
+
   return (
     <View
       style={{ ...StyleSheet.absoluteFillObject, backgroundColor: "black" }}
     >
-      <FocusFade>
-        <ScannerCamera />
-      </FocusFade>
-
+      {hasPermission && (
+        <FocusFade>
+          <ScannerCamera />
+        </FocusFade>
+      )}
       <ScanningIndicator />
     </View>
   );
